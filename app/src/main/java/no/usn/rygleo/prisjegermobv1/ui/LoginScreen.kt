b8b5be9d64c6990bed89c47848e1bc6ef7be37fb
tob12312 <78079618+tob12312@users.noreply.github.com>
2022-11-02 18:35:33 +0100

    Update LoginScreen.kt
    oppdatert loginscreen

--- app/src/main/java/no/usn/rygleo/prisjegermobv1/ui/LoginScreen.kt
+++ app/src/main/java/no/usn/rygleo/prisjegermobv1/ui/LoginScreen.kt
@@ -3,6 +3,7 @@ package no.usn.rygleo.prisjegermobv1.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -16,20 +17,55 @@ import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import no.usn.rygleo.prisjegermobv1.R
+import no.usn.rygleo.prisjegermobv1.ui.theme.Purple700
 
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 600)
 @Composable
-fun LoginScreen( ) {
+fun LoginScreen( prisjegerViewModel: PrisjegerViewModel) {
+    var isLoggedIn1 by remember{ mutableStateOf(false)}
+    var isLoggedIn by remember{ mutableStateOf(false)}
     var brukerNavn by remember{ mutableStateOf("")}
     var passord by remember{ mutableStateOf("")}
+    val openDialog = remember { mutableStateOf(false)}
+    var text by remember { mutableStateOf("")}
+
+
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                openDialog.value = false
+            },
+            title = {
+                Text(text = text)
+            },
+            buttons = {
+                Row(
+                    modifier = Modifier.padding(all = 8.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = { openDialog.value = false }
+                    ) {
+                        Text("Tilbake")
+                    }
+                }
+            }
+        )
+    }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -63,21 +99,23 @@ fun LoginScreen( ) {
               textAlign = TextAlign.Left,
               color = Color.Red
           )*/
-        OutlinedTextField(value = brukerNavn,
-            onValueChange = {brukerNavn=it},
-            label = { Text("brukernavn")},
-            leadingIcon =  {
-                Icon( Icons.Default.Person, contentDescription = "bruker")
+        OutlinedTextField(
+            value = brukerNavn,
+            onValueChange = { brukerNavn = it },
+            label = { Text("brukernavn") },
+            leadingIcon = {
+                Icon(Icons.Default.Person, contentDescription = "bruker")
             },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 10.dp, top = 10.dp)
         )
-        OutlinedTextField(value =passord ,
-            onValueChange = {passord=it},
-            label = { Text("passord")},
-            leadingIcon =  {
-                Icon( Icons.Default.Info, contentDescription = "password")
+        OutlinedTextField(
+            value = passord,
+            onValueChange = { passord = it },
+            label = { Text("passord") },
+            leadingIcon = {
+                Icon(Icons.Default.Info, contentDescription = "password")
             },
             modifier = Modifier
                 .fillMaxWidth()
@@ -85,22 +123,108 @@ fun LoginScreen( ) {
             visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
-        OutlinedButton(onClick = { /*TODO*/ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp, top = 10.dp),
-            elevation = ButtonDefaults.elevation(100.dp),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color.Blue,
-                contentColor = Color.White
-            )
-        ){
-            Text(
-                text = "login",
-                textAlign = TextAlign.Center)
+        if (isLoggedIn1 == false) {
+            OutlinedButton(
+                onClick = {
+                    if (brukerNavn.isNotEmpty() && passord.isNotEmpty()) {
+                        prisjegerViewModel.postAPILogin(brukerNavn, passord)
+                        if (prisjegerViewModel.brukerAPI.value?.get("melding")
+                                .equals("innlogget")
+                        ) {
+                            isLoggedIn = true
+                            text="logget inn"
+
+                        }
+                        else
+                        {
+                            isLoggedIn = false
+                            text= "Feil passord eller brukernavn"
+                        }
+                        //HVIS VELLYKKET = RES.JSON("MEDLING": 'INNLOGGET')
+                    }
+                    else{ text= "feil passord eller brukernavn"}
+                    openDialog.value=true   },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp, top = 10.dp),
+                elevation = ButtonDefaults.elevation(100.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Blue,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(
+                    text = "login",
+                    textAlign = TextAlign.Center
+                )
+
+            }
+            Column(
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+
+            ) {
+
+                ClickableText(
+                    text = AnnotatedString("Registrer ny bruker"),
+
+                    onClick = { isLoggedIn1 = true },
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily.Default,
+                        textDecoration = TextDecoration.Underline,
+                        color = Purple700
+                    )
+                )
+            }
+        }
+    else
+        {
+            OutlinedButton(
+                onClick = {
+                    if (brukerNavn.isNotEmpty() && passord.isNotEmpty()) {
+                        prisjegerViewModel.postAPIRegistrer(brukerNavn, passord)
+                        if (prisjegerViewModel.registrerAPI.value
+                                .equals("brukerEKS")
+                        ) {
+                            text = "bruker eksisterer allerede"
+
+                        }
+                        else {
+                            text = "bruker registrert"
+                        }
+                    }
+                    else text = "brukernavn og passord må ha verdi"
+                    openDialog.value=true
+                    isLoggedIn1 = false
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp, top = 10.dp),
+                elevation = ButtonDefaults.elevation(100.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Blue,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(
+                    text = "Registrer",
+                    textAlign = TextAlign.Center
+                )
+
+            }
 
         }
-
     }
 
+
+
 }
+@Composable
+fun Registrer(  ){
+
+}
+
+
